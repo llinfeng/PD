8dac757df836bea736eaff7b0e6e99a171195c16
llinfeng <lee.1900.cema@gmail.com>
2013-11-22 12:30:25 -0500

    Homework for week 12

--- assignment_1.py
+++ assignment_1.py
@@ -7,13 +7,7 @@ import numpy.random
 
 
 class SimpleGame(object):
-    '''
-    Simple game class.
-    This class is analogous to our Tournament object in the
-    Rock-Paper-Scissors tournament.
-    '''
-
-    # Payoff matrix;
+    # Payoff matrix; we didn't use Numpy here.  
     payoff_matrix = [[(3, 3), (0, 5)],
                     [(5, 0), (1, 1)]]
 
@@ -21,51 +15,42 @@ class SimpleGame(object):
     game_history = []
 
     def __init__(self, player_list):
-        '''
-        Constructor for the game; takes a list of players.
+        self.players = player_list
+        self.player1 = self.players[0]
+        self.player2 = self.players[1]
 
-        You need to make sure that any variables, e.g., player_list,
-        are made available to the class ongoing.
-        '''
-        pass
-
-    def payoff(self, moves):
-        '''
-        Payoff method given a set of moves.
-
-        This method needs to return the payoff value for a given
-        pair of moves; you should use the payoff_matrix defined
-        above.
-        '''
-        pass
+    def payoff(self):
+        move1 = self.player1.move() 
+        move2 = self.player2.move()
+        return self.payoff_matrix[move1][move2]
 
     def run(self):
         '''
-        Run method to run an actual game.
-
+        Run method to run an actua9
         You will need to do the following:
             1. Ask players for their moves
             2. Score the moves to determine their payoffs
             3. Update their scores
             4. Record their histories
         '''
-        pass
+        moves = [self.players[0].move(),self.players[1].move()]
+        score = self.payoff()
+        self.game_history.append([moves[0], moves[1]])
+        self.player1.record(moves,score)
+        self.player2.record(moves,score)
+        return [score]
 
-class RandomPlayer(object):
-    '''
-    RandomPlayer class is a simple PD player who
-    has a constant probability of defecting per game.
-    '''
+'''
+Given the current rule of payoff assignment, I can only come up with the following two-player-class 
+structure to keep track of game history.
 
-    # Default probability of defection
+'''
+
+class RandomPlayer1(object):
     probability_defect = 0.5
-
-    # Score
     score = 0.0
-
-    # History
     history = []
-
+    outcome = []
     def __init__(self, probability_defect):
         '''
         Constructor for the player; takes a probability
@@ -73,38 +58,84 @@ class RandomPlayer(object):
 
         You will need to set the class variable from the argument.
         '''
-        pass
+        self.probability_defect = probability_defect
 
     def move(self):
+        if numpy.random.random() <= self.probability_defect:
+            return 0 # this is playing C; if rand < probability_defect, not sufficient to defect. 
+        else:
+            return 1
+
+    def record(self,outcome,score):
+        self.outcome.append(score[0])
+        if self.move() < 0.5: 
+            self.history.append('C')
+        else:
+            self.history.append('D')
+
+class RandomPlayer2(object):
+    probability_defect = 0.5
+    score = 0.0
+    history = []
+    outcome = []
+    def __init__(self, probability_defect):
         '''
-        The move method returns the player's move based on a
-        random draw and their constant probability.
+        Constructor for the player; takes a probability
+        of defection as input.
 
-        You will need to:
-            1. Draw a random variate from numpy.random.random()
-            2. Transform the value into an action
+        You will need to set the class variable from the argument.
         '''
-        pass
+        self.probability_defect = probability_defect
 
-    def record(self, outcome, score):
-        '''
-        The record method allows the player to update their score
-        and history values if desired.
-        '''
-        pass
+    def move(self):
+        if numpy.random.random() <= self.probability_defect:
+            return 0 # this is playing C; if rand < probability_defect, not sufficient to defect. 
+        else:
+            return 1
+
+    def record(self,outcome,score):
+        self.outcome.append(score[1])
+        if self.move() < 0.5: 
+            self.history.append('C')
+        else:
+            self.history.append('D')
 
 
-if __name__ == "__main__":
-    '''
-    Sample driver.
-    '''
-    # Create players
-    player_1 = RandomPlayer(0.5)
-    player_2 = RandomPlayer(0.25)
 
-    # Create game
-    simple_game = SimpleGame([player_1, player_2])
 
-    # Run the game
-    result = simple_game.run()
-    print result
+
+player_1 = RandomPlayer1(0.5)
+player_2 = RandomPlayer2(0.25)
+print player_2.move()
+playerlist = [player_1, player_2]
+
+# Create game
+SG = SimpleGame(playerlist)
+result = SG.run()
+print result
+
+
+print 'player 1 played ' + str(SG.player1.history) + "and got" + str(SG.player1.outcome)
+print SG.player1.move() 
+
+print 'player 2 played ' + str(SG.player2.history) + "and got" + str(SG.player2.outcome)
+print SG.player2.move()
+
+
+'''
+Remark after setting up the code
+
+    1. Accessing the game history: to access the history/record of a single SG, need access 
+    through the mother class. 
+
+    2. Using the generating player will yield another random draw, and therefore the strategy
+    returned may not necessarily be the one played by the players in the game. 
+
+    3. Use self.XXX when referring to class instance, or else python don't take XXX as a globle name 
+    and won't be notified what it is. 
+
+    To conclude: it took me 3 hours to figure all the above, sadly. The usage of "outcome" and "history" and 
+    "score" are confusing. It would be better to layout a specs explaining what each variable does. 
+
+    
+'''
--- assignment_2.py
+++ assignment_2.py
@@ -5,19 +5,30 @@ More advanced PD game model, using either:
 to drive the interactions of players.
 '''
 
+'''
+Useful links: 
+http://networkx.github.io/documentation/latest/reference/functions.html#module-networkx.classes.function
+
+'''
+
 # Required imports
-import numpy
-import networkx
+import numpy as np
+import itertools
+import random
+import math
+import networkx as nx
+from networkx.generators.classic import empty_graph, path_graph, complete_graph
+from collections import defaultdict
 
 
 class Grid2D(object):
     '''
     The Grid2D class represents a simple two-dimensional grid.
     '''
-
     # Dimensions of the grid
     num_rows = 0
     num_cols = 0
+    radius = 3 # Neighborhood radius
 
     def __init__(self, num_rows, num_cols):
         '''
@@ -25,10 +36,14 @@ class Grid2D(object):
         and columns.
 
         Your task is to create either:
-            1. A list of lists
-            2. A numpy.array
+            1. A list of lists; this is comma seperated;
+            2. A numpy.array; this would be a vector like stuff.
         '''
-        pass
+        self.num_rows = num_rows
+        self.num_cols = num_cols
+        self.grid, self.grid = np.mgrid[0:num_rows,0:num_cols] # The last entry in the list\
+                                            # would be (num_rows-1, num_cols-1)
+
 
     def put(self, row, col, obj):
         '''
@@ -37,7 +52,7 @@ class Grid2D(object):
         You should do this by assigning a value to your list-of-lists or
         numpy.array with the proper index.
         '''
-        pass
+        self.grid[row-1,col-1] = obju
 
     def get(self, row, col):
         '''
@@ -46,7 +61,7 @@ class Grid2D(object):
         You should do this by returning the value from your list-of-lists or
         numpy.array with the proper index.
         '''
-        pass
+        return self.grid(row-1,col-1)
 
     def get_neighbors(self, row, col):
         '''
@@ -56,7 +71,14 @@ class Grid2D(object):
         For extra challenge, add an argument that allows either von Neumann
         or Moore neighborhoods.
         '''
-        pass
+        self.get(row,col).neighbors = []
+        for i in range(num_rows):
+            for j in range(num_cols):
+                if (i-row)**2 + (j-col)**2 < radius**2:
+                    self.get(row,col).neighbors.append([i,j])
+
+        return self.get(row,col).neighbors
+
 
 
 class RandomNetwork(object):
@@ -79,26 +101,29 @@ class RandomNetwork(object):
             * http://networkx.github.io/documentation/latest/tutorial/index.html
             * http://networkx.github.io/documentation/latest/reference/generators.html
         '''
-        pass
+        self.RN = gnm_random_graph(num_nodes,num_edges)
 
     def put(self, node_id, obj):
         '''
         The put method places an object in a given node_id.
-        '''
-        pass
 
-    def get(self, row, col):
+        Node_id starts from 0;
+        '''
+        self.RN.nodes()[node_id].append(obj)
+
+
+    def get(self, node_id):
         '''
         The get method returns an object in a given node_id.
         '''
-        pass
+        return self.RN
 
     def get_neighbors(self, node_id):
         '''
         The get_neighbors method returns the list of adjacent nodes for
         a given node_id.
         '''
-        pass
+        return nx.set_node_attributes(RN,self.RN.neighbors(node_id),bb)
 
 
 class RandomPlayer(object):
@@ -125,7 +150,7 @@ class RandomPlayer(object):
 
         You will need to set the class variable from the argument.
         '''
-        pass
+        self.probability_defect = probability_defect
 
     def move(self):
         '''
@@ -136,11 +161,66 @@ class RandomPlayer(object):
             1. Draw a random variate from numpy.random.random()
             2. Transform the value into an action
         '''
-        pass
+        if numpy.random.random() <= self.probability_defect:
+            return 0 # this is playing C; if rand < probability_defect, not sufficient to defect. 
+        else:
+            return 1
 
     def record(self, outcome, score):
         '''
         The record method allows the player to update their score
         and history values if desired.
+
+        I don't see what exactly "outcome" and "score" means. Should the player keep track of his opponents' strategy and score?
         '''
         pass
+
+
+
+def gnm_random_graph(n, m, seed=None, directed=False):
+    """Return the random graph G_{n,m}.
+
+    Produces a graph picked randomly out of the set of all graphs
+    with n nodes and m edges.
+
+    Parameters
+    ----------
+    n : int
+        The number of nodes.
+    m : int
+        The number of edges.
+    seed : int, optional
+        Seed for random number generator (default=None).
+    directed : bool, optional (default=False)
+        If True return a directed graph
+    """
+    if directed:
+        G=nx.DiGraph()
+    else:
+        G=nx.Graph()
+    G.add_nodes_from(range(n))
+    G.name="gnm_random_graph(%s,%s)"%(n,m)
+
+    if seed is not None:
+        random.seed(seed)
+
+    if n==1:
+        return G
+    max_edges=n*(n-1)
+    if not directed:
+        max_edges/=2.0
+    if m>=max_edges:
+        return complete_graph(n,create_using=G)
+
+    nlist=G.nodes()
+    edge_count=0
+    while edge_count < m:
+        # generate random edge,u,v
+        u = random.choice(nlist)
+        v = random.choice(nlist)
+        if u==v or G.has_edge(u,v):
+            continue
+        else:
+            G.add_edge(u,v)
+            edge_count=edge_count+1
+    return G
